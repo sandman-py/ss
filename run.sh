0e6badb67f88a5a8e70a4344d06f3c306cf174ad
sandman-py <puyuan001@126.com>
2017-03-10 22:26:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nohup  ssserver -p 443 -k   -m aes-256-cfb --user nobody -d start --log-file ss.log  2>&1 &
+nohup  ssserver -p 443 -k  123 -m aes-256-cfb --user nobody -d start --log-file ss.log  2>&1 &
